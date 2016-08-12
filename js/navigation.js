a32210cd9338347e10462f631471073f5f950e41
jagruti <jagruti@wohlig.com>
2016-08-12 17:44:18 +0530

    filter

--- js/navigation.js
+++ js/navigation.js
@@ -1,4 +1,4 @@
-var adminurl = "http://192.168.1.113:1337/";
+var adminurl = "http://192.168.1.113:80/";
 var imgurl = adminurl + "upload/";
 var imgpath = imgurl + "readFile";
 var uploadurl = imgurl;
@@ -14,112 +14,145 @@ var navigationservice = angular.module('navigationservice', [])
 
 .factory('NavigationService', function($http) {
 
-  var navigation = [{
-    name: "Skin",
-    classis: "active",
-    anchor: "skin",
-    colours:"borskin"
-  },{
-    name: "Hair & Scalp",
-    classis: "active",
-    anchor: "hairscalp",
-    colours:"borhair",
-    subnav: [{
-      name: "Hair Loss",
-      classis: "active",
-      anchor: "hair"
-    },{
-      name: "Dandruff",
-      classis: "active",
-      anchor: "dandruff"
-    },{
-      name: "Weathered Hair",
-      classis: "active",
-      anchor: "weatheredhair"
-    },{
-      name: "Hair Maintenance",
-      classis: "active",
-      anchor: "hairmaintenance"
-    }]
-  },{
-    name: "Body",
-    classis: "active",
-    anchor: "body",
-    colours:"borbody"
-  },{
-    name: "Men",
-    classis: "active",
-    anchor: "men",
-    colours:"bormen"
-  },{
-    name: "Treatments",
-    classis: "active",
-    anchor: "treatment",
-    colours:"bortreat"
-  },{
-    name: "Mummy Derm",
-    classis: "active",
-    anchor: "mummyderm",
-    colours:"borderm"
-  },{
-    name: "Vitiligo",
-    classis: "actives",
-    anchor: "vitiligo",
-    colours:"borviti"
-  }];
+    var navigation = [{
+        name: "Skin",
+        classis: "active",
+        anchor: "skin",
+        colours: "borskin"
+    }, {
+        name: "Hair & Scalp",
+        classis: "active",
+        anchor: "hairscalp",
+        colours: "borhair",
+        subnav: [{
+            name: "Hair Loss",
+            classis: "active",
+            anchor: "hair"
+        }, {
+            name: "Dandruff",
+            classis: "active",
+            anchor: "dandruff"
+        }, {
+            name: "Weathered Hair",
+            classis: "active",
+            anchor: "weatheredhair"
+        }, {
+            name: "Hair Maintenance",
+            classis: "active",
+            anchor: "hairmaintenance"
+        }]
+    }, {
+        name: "Body",
+        classis: "active",
+        anchor: "body",
+        colours: "borbody"
+    }, {
+        name: "Men",
+        classis: "active",
+        anchor: "men",
+        colours: "bormen"
+    }, {
+        name: "Treatments",
+        classis: "active",
+        anchor: "treatment",
+        colours: "bortreat"
+    }, {
+        name: "Mummy Derm",
+        classis: "active",
+        anchor: "mummyderm",
+        colours: "borderm"
+    }, {
+        name: "Vitiligo",
+        classis: "actives",
+        anchor: "vitiligo",
+        colours: "borviti"
+    }];
 
-  return {
-    getnav: function() {
-      return navigation;
-    },
+    return {
+        getnav: function() {
+            return navigation;
+        },
 
-    makeactive: function(menuname) {
-      for (var i = 0; i < navigation.length; i++) {
-        if (navigation[i].name == menuname) {
-          navigation[i].classis = "active";
-        } else {
-          navigation[i].classis = "";
-        }
-      }
-      return menuname;
-    },
-    getHomeslider: function(callback) {
+        makeactive: function(menuname) {
+            for (var i = 0; i < navigation.length; i++) {
+                if (navigation[i].name == menuname) {
+                    navigation[i].classis = "active";
+                } else {
+                    navigation[i].classis = "";
+                }
+            }
+            return menuname;
+        },
+        getHomeslider: function(callback) {
             $http({
-            url: adminurl + 'Homeslider/getAll',
-            method: 'POST',
-            withCredentials: true
-        }).success(callback);
-    },
-    getAllCategory: function(callback) {
+                url: adminurl + 'Homeslider/getAll',
+                method: 'POST',
+                withCredentials: true
+            }).success(callback);
+        },
+        getAllCategory: function(callback) {
             $http({
-            url: adminurl + 'Category/getAllCat',
-            method: 'POST',
-            withCredentials: true
-        }).success(callback);
-    },
-getCatByName:function(id,callback){
-  $http({
-    url:adminurl+'SubCategory/getCatByName',
-    method:'POST',
-    withCredentials:true,
-    data: {
-        _id: id
-    }
-  }).success(callback);
-},
-getSubCat:function(id,callback){
-  $http({
-    url:adminurl+'SubCategory/getOne',
-    method:'POST',
-    withCredentials:true,
-    data: {
-        _id: id
-    }
-  }).success(callback);
-},
+                url: adminurl + 'Category/getAllCat',
+                method: 'POST',
+                withCredentials: true
+            }).success(callback);
+        },
+        getCatByName: function(id, callback) {
+            $http({
+                url: adminurl + 'SubCategory/getCatByName',
+                method: 'POST',
+                withCredentials: true,
+                data: {
+                    _id: id
+                }
+            }).success(callback);
+        },
+        getSubCat: function(id, callback) {
+            $http({
+                url: adminurl + 'SubCategory/getOne',
+                method: 'POST',
+                withCredentials: true,
+                data: {
+                    _id: id
+                }
+            }).success(callback);
+        }, // GET ALL BLOG BY JAGRUTI
+        getAllBlog: function(pagenumber, search, callback) {
+            $http({
+                url: adminurl + 'blog/getLimited',
+                method: 'POST',
+                data: {
+                    "pagesize": 2,
+                    "pagenumber": pagenumber,
+                    "search": search
+                },
+                withCredentials: true
+            }).success(callback);
+        },
+        getPostTags: function(callback) {
+            $http({
+                url: adminurl + 'blog/getPostTags',
+                method: 'POST',
+                withCredentials: true
+            }).success(callback);
+        },
+        getOneBlog: function(id, callback) {
+            $http({
+                url: adminurl + 'blog/getOneBlog',
+                method: 'POST',
+                data: {
+                    "_id": id
+                },
+                withCredentials: true
+            }).success(callback);
+        },
+        getPopularPosts: function(callback) {
+            $http({
+                url: adminurl + 'blog/getPopularPosts',
+                method: 'POST',
+                withCredentials: true
+            }).success(callback);
+        }, // GET ALL BLOG BY JAGRUTI
 
-
-
-
-  };
+    };
 });
